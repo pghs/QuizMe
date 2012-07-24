8f627666c331cef9aeca22f09febbe366ff377c6
Jason Urton <jason.urton@gmail.com>
2012-07-24 15:01:27 -0400

    remove correct to mention migration

--- db/migrate/20120724181728_add_correct_to_mentions.rb
+++
@@ -1,5 +0,0 @@
-class AddCorrectToMentions < ActiveRecord::Migration
-  def change
-    add_column :mentions, :correct, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,53 +11,52 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724181728) do
+ActiveRecord::Schema.define(:version => 20120723212146) do
 
   create_table "accounts", :force => true do |t|
-    t.string   "name"
-    t.string   "twi_name"
-    t.string   "twi_screen_name"
-    t.integer  "twi_user_id"
-    t.text     "twi_profile_img_url"
-    t.string   "twi_oauth_token"
-    t.string   "twi_oauth_secret"
-    t.string   "fb_oauth_token"
-    t.string   "fb_oauth_secret"
-    t.string   "tum_oauth_token"
-    t.string   "tum_oauth_secret"
-    t.string   "tum_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "posts_per_day",       :default => 1
-    t.boolean  "link_to_quizme",      :default => false
+    t.string    "name"
+    t.string    "twi_name"
+    t.string    "twi_screen_name"
+    t.integer   "twi_user_id"
+    t.text      "twi_profile_img_url"
+    t.string    "twi_oauth_token"
+    t.string    "twi_oauth_secret"
+    t.string    "fb_oauth_token"
+    t.string    "fb_oauth_secret"
+    t.string    "tum_oauth_token"
+    t.string    "tum_oauth_secret"
+    t.string    "tum_url"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "posts_per_day",       :default => 1
+    t.text      "description"
   end
 
   create_table "accountstopics", :force => true do |t|
-    t.integer  "account_id"
-    t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "account_id"
+    t.integer   "topic_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "answers", :force => true do |t|
-    t.boolean  "correct"
-    t.integer  "question_id"
-    t.text     "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.boolean   "correct"
+    t.integer   "question_id"
+    t.text      "text"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "mentions", :force => true do |t|
-    t.integer   "user_id"
-    t.integer   "post_id"
-    t.text      "text"
-    t.boolean   "responded",                 :default => false
-    t.string    "twi_tweet_id"
-    t.string    "twi_in_reply_to_status_id"
-    t.timestamp "sent_date"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.boolean   "correct"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "text"
+    t.boolean  "responded",                 :default => false
+    t.string   "twi_tweet_id"
+    t.string   "twi_in_reply_to_status_id"
+    t.datetime "sent_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "post_queues", :force => true do |t|
@@ -69,35 +68,33 @@ ActiveRecord::Schema.define(:version => 20120724181728) do
   end
 
   create_table "posts", :force => true do |t|
-    t.integer  "account_id"
-    t.integer  "question_id"
-    t.string   "provider"
-    t.text     "text"
-    t.string   "url"
-    t.string   "link_type"
-    t.string   "post_type"
-    t.string   "provider_post_id"
-    t.integer  "to_twi_user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "account_id"
+    t.integer   "question_id"
+    t.string    "provider"
+    t.text      "text"
+    t.string    "url"
+    t.string    "link_type"
+    t.string    "post_type"
+    t.string    "provider_post_id"
+    t.integer   "to_twi_user_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "questions", :force => true do |t|
-    t.text     "text"
-    t.string   "url"
-    t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "qb_lesson_id"
-    t.integer  "qb_q_id"
+    t.text      "text"
+    t.string    "url"
+    t.integer   "topic_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "reps", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.boolean  "correct"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "user_id"
+    t.integer   "post_id"
+    t.boolean   "correct"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "stats", :force => true do |t|
@@ -124,22 +121,22 @@ ActiveRecord::Schema.define(:version => 20120724181728) do
   end
 
   create_table "topics", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "name"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "twi_name"
-    t.string   "twi_screen_name"
-    t.integer  "twi_user_id"
-    t.text     "twi_profile_img_url"
-    t.string   "twi_oauth_token"
-    t.string   "twi_oauth_secret"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "provider"
-    t.integer  "uid"
+    t.string    "twi_name"
+    t.string    "twi_screen_name"
+    t.integer   "twi_user_id"
+    t.text      "twi_profile_img_url"
+    t.string    "twi_oauth_token"
+    t.string    "twi_oauth_secret"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "provider"
+    t.integer   "uid"
   end
 
 end
